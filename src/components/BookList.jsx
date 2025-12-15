65a5c4a0d6b25472eb08eccc36faece21a4c3446
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-15 15:13:51 -0500

    Update BookList.jsx

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,1 +1,14 @@
+import { Grid } from "@mui/material";
+import BookCard from "./BookCard";
 
+export default function BookList({ books }) {
+  return (
+    <Grid container spacing={3}>
+      {books.map((book) => (
+        <Grid item xs={12} sm={6} md={4} key={book.id}>
+          <BookCard book={book} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
