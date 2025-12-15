d0b248e26bdb9f4b61cdb7ce20455605fbef2780
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-15 16:05:51 -0500

    Update BookList.jsx

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,9 +3,21 @@ import BookCard from "./BookCard";
 
 export default function BookList({ books }) {
   return (
-    <Grid container spacing={3}>
+    <Grid
+      container
+      spacing={4}
+      justifyContent="center"
+    >
       {books.map((book) => (
-        <Grid item xs={12} sm={6} md={4} key={book.id}>
+        <Grid
+          item
+          key={book.id}
+          xs={12}
+          sm={6}
+          md={4}
+          lg={3}
+          sx={{ display: "flex", justifyContent: "center" }}
+        >
           <BookCard book={book} />
         </Grid>
       ))}
