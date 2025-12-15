037dc07b423fbe43be28e0429f145ed9f5110b4a
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-15 15:13:29 -0500

    Update BookCard.jsx

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,1 +1,31 @@
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Rating,
+  Stack,
+} from "@mui/material";
 
+export default function BookCard({ book }) {
+  return (
+    <Card sx={{ height: "100%" }}>
+      <CardMedia
+        component="img"
+        height="220"
+        image={book.image}
+        alt={book.title}
+      />
+      <CardContent>
+        <Stack spacing={1}>
+          <Typography variant="h6">{book.title}</Typography>
+          <Typography variant="body2" color="text.secondary">
+            {book.author}
+          </Typography>
+          <Typography variant="caption">{book.genre}</Typography>
+          <Rating value={book.rating} readOnly />
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+}
