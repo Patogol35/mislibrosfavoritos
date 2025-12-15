7bdd0cb73716c69b0a68c4b782a97fa34a4c190e
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-15 15:13:07 -0500

    Update books.js

--- src/data/books.js
+++ src/data/books.js
@@ -1,1 +1,18 @@
-
+export const initialBooks = [
+  {
+    id: 1,
+    title: "1984",
+    author: "George Orwell",
+    genre: "Distopía",
+    rating: 5,
+    image: "https://covers.openlibrary.org/b/id/7222246-L.jpg",
+  },
+  {
+    id: 2,
+    title: "El Principito",
+    author: "Antoine de Saint-Exupéry",
+    genre: "Fantasía",
+    rating: 5,
+    image: "https://covers.openlibrary.org/b/id/9255566-L.jpg",
+  },
+];
