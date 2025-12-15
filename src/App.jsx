de4249e0f0bf65973256928c3aab2021af544c08
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-15 16:17:49 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import {
   Stack,
   Divider,
   TextField,
+  IconButton,
 } from "@mui/material";
 import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
 import { useState } from "react";
 import BookList from "./components/BookList";
 import { initialBooks } from "./data/books";
 
-export default function App() {
+export default function App({ mode, setMode }) {
   const [filter, setFilter] = useState("");
 
   const filteredBooks = initialBooks.filter((book) =>
@@ -22,14 +25,29 @@ export default function App() {
       {/* Header */}
       <Stack
         direction="row"
-        spacing={1.5}
         alignItems="center"
+        justifyContent="space-between"
         mb={2}
       >
-        <AutoStoriesIcon sx={{ fontSize: 32 }} />
-        <Typography variant="h4" fontWeight={700}>
-          Mis libros favoritos
-        </Typography>
+        <Stack direction="row" spacing={1.5} alignItems="center">
+          <AutoStoriesIcon sx={{ fontSize: 32 }} />
+          <Typography variant="h4" fontWeight={700}>
+            Mis libros favoritos
+          </Typography>
+        </Stack>
+
+        {/* TOGGLE */}
+        <IconButton
+          onClick={() =>
+            setMode(mode === "light" ? "dark" : "light")
+          }
+        >
+          {mode === "light" ? (
+            <DarkModeIcon />
+          ) : (
+            <LightModeIcon />
+          )}
+        </IconButton>
       </Stack>
 
       <Typography color="text.secondary" mb={3}>
@@ -39,11 +57,10 @@ export default function App() {
       {/* Filtro */}
       <TextField
         label="Buscar por autor"
-        variant="outlined"
         fullWidth
         value={filter}
         onChange={(e) => setFilter(e.target.value)}
-        sx={{ mb: 4, maxWidth: 360 }}
+        sx={{ maxWidth: 360, mb: 4 }}
       />
 
       <Divider sx={{ mb: 4 }} />
