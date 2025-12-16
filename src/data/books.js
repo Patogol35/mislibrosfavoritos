a189991d5464b5cbaa8a1b0e349a6d3c2d1efc49
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-16 09:32:22 -0500

    Update books.js

--- src/data/books.js
+++ src/data/books.js
@@ -1,54 +1,53 @@
+// src/data/books.js
+
 export const initialBooks = [
   {
     id: 1,
     title: "El Último Secreto",
     author: "Dan Brown",
     image: "https://res.cloudinary.com/dqkwc0kf7/image/upload/v1765840460/img20250909_16353238--1-_n8kosu.jpg",
+    status: "reading", // leído
   },
   {
     id: 2,
     title: "Macara: Memorias de una Bandita Enfermedad",
     author: "Jefferson Pérez Aguachela",
     image: "https://res.cloudinary.com/dqkwc0kf7/image/upload/v1765839617/62650_rkkxmf.jpg",
+    status: "reading", // en proceso
   },
   {
     id: 3,
     title: "El Código Da Vinci",
     author: "Dan Brown",
-    genre: "Thriller",
-    rating: 4,
-    image: "https://covers.openlibrary.org/b/id/240726-L.jpg",
+    image: "https://res.cloudinary.com/dqkwc0kf7/image/upload/v1765895453/176407_portada_el-codigo-da-vinci_dan-brown_201505260959_lxtte8.jpg",
+    status: "read",
   },
   {
     id: 4,
     title: "El Símbolo Perdido",
     author: "Dan Brown",
-    genre: "Thriller",
-    rating: 4,
     image: "https://res.cloudinary.com/dqkwc0kf7/image/upload/v1765839418/images_yxpt4i.jpg",
+    status: "read",
   },
   {
     id: 5,
-    title: "El Origen",
+    title: "Origen",
     author: "Dan Brown",
-    genre: "Thriller",
-    rating: 4,
     image: "https://covers.openlibrary.org/b/id/9255896-L.jpg",
+    status: "read", // no leído
   },
   {
     id: 6,
     title: "El Kybalion",
     author: "Tres Iniciados",
-    genre: "Filosofía",
-    rating: 5,
     image: "https://covers.openlibrary.org/b/id/10523357-L.jpg",
+    status: "read",
   },
   {
     id: 7,
     title: "El Corán",
     author: "Anónimo",
-    genre: "Religión",
-    rating: 5,
     image: "https://covers.openlibrary.org/b/id/8231851-L.jpg",
+    status: "reading",
   },
 ];
