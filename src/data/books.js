dc221e9579a891143f8477a7ce7eb1e2172cc90e
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-17 23:04:09 -0500

    Update books.js

--- src/data/books.js
+++ src/data/books.js
@@ -80,7 +80,7 @@ export const initialBooks = [
     id: 12,
     title: "Solo para tus ojos",
     author: "J. J. Benítez",
-    image: "https://covers.openlibrary.org/b/id/10909286-L.jpg",
-    status: "unread",
+    image: "https://res.cloudinary.com/dqkwc0kf7/image/upload/v1766030561/dd56ff55ef5963add5e7c287b354852d_iz7orl.jpg",
+    status: "reading",
   },
 ];
